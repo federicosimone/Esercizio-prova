22716a7a4be15b173f147cdbbf215cd1f9829801
federicosimone <federico.simone15@gmail.com>
2025-12-01 11:34:15 +0100

    js1

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 axios.get("https://mocki.io/v1/8da7f3a9-881b-43d4-8d32-d0bb693ce8e7").then( risposta => {
     console.log(risposta.data)
-})
+    const prodotti = risposta.data ;
+
+    let cardsContainer = document.getElementById("cards-container") ;
+
+    let html = "" ;
+
+    for (let i = 0; i < prodotti.length; i++) {
+        const element = prodotti[i];
+
+        let img = element.immagine ;
+        let nome = element.nome ;
+        let prezzo = element.prezzo ;
+        let descrizione = element.descrizone ;
+        
+    }
+
+
+
+
+
+
+
+
+
+}) ;
+
